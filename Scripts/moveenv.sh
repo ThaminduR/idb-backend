2562724c7ecd17cd3748ae7712a329923f9a23ff
ThaminduR <thamindu.randil@gmail.com>
2020-07-24 21:50:12 +0530

    minor error in bash

--- Scripts/moveenv.sh
+++ Scripts/moveenv.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-cd /home/ubuntu/idb/
-sudo cp .env /home/ubuntu/idb/idb-backend
+cd /home/ubuntu/idb/idb-backend
+sudo cp /home/ubuntu/idb/.env /home/ubuntu/idb/idb-backend
 sudo pm2 start index.js
