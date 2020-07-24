9af9f9ad58815825f6576622f6bbb59fbd55b59c
ThaminduR <thamindu.randil@gmail.com>
2020-07-25 00:42:03 +0530

    login middleware removed

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,7 +4,7 @@ const auth = require('../models/userauth')
 const middleware = require('../models/middleware')
 const userrouter = require('./routes')
 
-router.post('/login', [middleware.notloggedin], (req, res) => { auth.login(req, res) })
+router.post('/login', (req, res) => { auth.login(req, res) })
 // router.post('/register', middleware.notloggedin, (req, res) => { auth.register(req, res) })
 router.get('/logout', [middleware.verifytoken], (req, res) => { auth.logout(req, res) })
 
